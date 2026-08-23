e9d64ed5fb87ec23c5d60ac58e0b8e3e50bb6036
even <even53330@gmail.com>
2026-08-23 19:42:50 +0200

    .

---
+++ leason_1.py
@@ -0,0 +1,7 @@
+print("Hello World", 4, 4 ,"skldfj",sep="", end="   ALILUYA \n")
+print("Sekod \" line")
+print('esdf ""dsf\td\\')
+print(5 + 5, 5*5, 5**3,5/3,5//3)
+print(min(1,2,3,4 , -2), max(1,2,3,4 , -2, abs(-6)), abs(-5), pow (5, 3), round(5/3))
+
+input("Hvor gammael er du? " )
