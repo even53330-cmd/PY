8fee53f7d15d011593d32a43e73c4ac24aba9be7
even <even53330@gmail.com>
2026-08-23 23:43:01 +0200

    .

---
+++ leason_2.py
@@ -0,0 +1,30 @@
+# number = 5 #int
+#
+# digit = 4.5324 #float
+# word = "hello" #string
+# print (str(number))
+# print (digit )
+# booless = True #bool
+# falll = False
+# print(booless, falll)
+#
+# print(digit, word)
+#
+# print("resultat",number)
+#
+# number = 7
+# print("resultat",number)
+# del number
+num1 = int(input("tast første talle "))
+num2 = int(input("taste andre talle "))
+
+num1 += num2
+
+print("Result", num1 + num2)
+print("Result", num1 / num2)
+print("Result", num1 * num2)
+print("Result", num1 - num2)
+word = "hi"
+print(word * 2)
+word = "LOL"
+print(word * 3)
